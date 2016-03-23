56ac87b3836c6825cb32329695bde78a6d5f8fdf
tangju0418 <635465547@qq.com>
2016-03-23 12:20:09 +0800

    列表，循环播放

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 window.onload=function(){
     mv.app.toTip();
     mv.app.toBanner();
+    mv.app.toSel();
+    mv.app.toRun();
 };
 var mv={};  //命名空间
 
@@ -78,6 +80,21 @@ mv.ui.fadeOut = function(obj){
     },30);
 };
 
+mv.ui.moveLeft = function(obj,old,now){
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function(){
+        var iSpeed = (now - old)/10;
+        iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+        if(now == old){
+            clearInterval(obj.timer);
+        }
+        else{
+            old += iSpeed;
+            obj.style.left = old +'px';
+        }
+    },30);
+};
+
 mv.app={};
 
 mv.app.toTip= function () {
@@ -145,5 +162,81 @@ mv.app.toBanner = function(){
     };
     oNext.onclick = function(){
         auto();
+    };
+};
+mv.app.toSel = function(){
+    var oSel = document.getElementById('sell');
+    var aDd = oSel.getElementsByTagName('dd');
+    var aUl = oSel.getElementsByTagName('ul');
+    var aH2 = oSel.getElementsByTagName('h2');
+
+    for(var i=0;i<aDd.length;i++){
+        aDd[i].index = i;
+        aDd[i].onclick = function(ev){
+            var ev = ev || window.event;
+            var This = this;
+            for(var i=0;i<aUl.length;i++){
+                aUl[i].style.display = 'none';
+            }
+            aUl[this.index].style.display='block';
+            document.onclick = function(){
+                aUl[This.index].style.display = 'none';
+            };
+            ev.cancelBubble = true;
+        }
+    }
+    for(var j=0;j<aUl.length;j++){
+        aUl[j].index = j;
+        (function(ul){
+            var aLi = ul.getElementsByTagName('li');
+            for(var i = 0;i<aLi.length;i++){
+                aLi[i].onmouseover = function(){
+                    this.className = 'active';
+                };
+                aLi[i].onmouseout = function(){
+                    this.className = '';
+                };
+                aLi[i].onclick = function(ev){
+                    var ev = ev || window.event;
+                    aH2[this.parentNode.index].innerHTML = this.innerHTML;
+                    ev.cancelBubble = true;
+                    this.parentNode.style.display = 'none';
+                }
+            }
+        })(aUl[j]);
+    }
+};
+
+mv.app.toRun = function() {
+    var oRun = document.getElementById('run1');
+    var oUl = oRun.getElementsByTagName('ul')[0];
+    var aLi = oUl.getElementsByTagName('li');
+    var oPrev = mv.tools.getByClass(oRun,'prev')[0];
+    var oNext = mv.tools.getByClass(oRun,'next')[0];
+    var iNow = 0;
+
+    oUl.innerHTML += oUl.innerHTML;
+    oUl.style.width = aLi.length * aLi[0].offsetWidth + 'px';
+
+    oPrev.onclick = function () {
+        if (iNow == 0) {
+            iNow = aLi.length / 2;
+            oUl.style.left = -oUl.offsetWidth / 2 + 'px';
+        }
+        mv.ui.moveLeft(oUl, -iNow * aLi[0].offsetWidth, -(iNow - 1) * aLi[0].offsetWidth);
+        iNow--;
+
+    };
+    oNext.onclick = function(){
+
+        if(iNow == aLi.length/2){
+            iNow = 0;
+            oUl.style.left = 0;
+        }
+
+        mv.ui.moveLeft(oUl,-iNow*aLi[0].offsetWidth,-(iNow+1)*aLi[0].offsetWidth);
+
+        iNow++;
+
     };
 };
